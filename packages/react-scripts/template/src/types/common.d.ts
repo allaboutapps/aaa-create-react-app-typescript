5ee7a84f579abd896a3de04e811c393f32beb094
Mario Ranftl <code@mranftl.com>
2017-10-17 20:14:06 +0200

    rm working common.d.ts fail

--- packages/react-scripts/template/src/types/common.d.ts
+++ packages/react-scripts/template/src/types/common.d.ts
@@ -1,6 +1,2 @@
 declare module "react-animations/*";
-
 declare module "hoist-non-react-statics";
-
-// this module is legacy and has known issues with newer react typings
-declare module "react-addons-linked-state-mixin";
