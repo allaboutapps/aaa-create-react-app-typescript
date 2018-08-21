2a90b367b67b0a6b371201563840c120f6c07fd4
Christoph Werner <christoph@chrztoph.com>
2018-08-21 16:18:28 +0200

    Improve default localizations

--- packages/react-scripts/template/src/i18n/en.ts
+++ packages/react-scripts/template/src/i18n/en.ts
@@ -1,6 +1,12 @@
+import deLocale from "./de";
+
 const locale = {
-    "main.welcomeText": `Welcome to React`,
-    "main.subText": `To get started, edit {location} and save to reload.`
+    ...deLocale,
+    "screen.not_found.title": "The site could not be found.",
+    "screen.login.invalid_password_or_email": "The password or email address is invalid.",
+    "screen.login.error_during_login": "An error occured while logging in.",
+    "screen.login.form.email.validation_error": "Enter a valid email address",
+    "screen.login.form.password.validation_error": "Enter a valid password"
 };
 
 export type IAvailableI18nIds = typeof locale;
--- packages/react-scripts/template/src/i18n/util.ts
+++ packages/react-scripts/template/src/i18n/util.ts
@@ -2,13 +2,9 @@ import * as hoistNonReactStatic from "hoist-non-react-statics";
 import * as RI from "react-intl";
 import { IAvailableI18nIds } from "./en";
 
-// helper to ensure that a specific i18n id is available 
+// helper to ensure that a specific i18n id is available
 export type IDS = keyof IAvailableI18nIds;
 export const id = (i18nId: IDS) => i18nId;
-
-// constructor type helper 
-// TODO: can go away with TypeScript 2.2 (global constructor type | primitive object type)
-type C<T> = new () => T;
 
 // proxy injectIntl through hoist-non-react-statics
 // react-intl is insane and not complying to conventions in the ecosystem, thus we handle this here directly.
@@ -17,13 +13,14 @@ type C<T> = new () => T;
 // see https://github.com/mridgway/hoist-non-react-statics
 export function injectIntl(WrappedComponent: any) {
     const InjectIntl = RI.injectIntl(WrappedComponent);
+
     return hoistNonReactStatic(InjectIntl, WrappedComponent);
 }
 
 // reexpose RI utils with limited available id settings
-export const FormattedHTMLMessage: C<RI.FormattedHTMLMessage<IDS>> = RI.FormattedHTMLMessage;
-export const FormattedMessage: C<RI.FormattedMessage<IDS>> = RI.FormattedMessage;
-export const FormattedPlural: C<RI.FormattedPlural<IDS>> = RI.FormattedPlural;
+export const FormattedHTMLMessage = RI.FormattedHTMLMessage;
+export const FormattedMessage = RI.FormattedMessage;
+export const FormattedPlural = RI.FormattedPlural;
 export const FormattedNumber = RI.FormattedNumber;
 export const FormattedDate = RI.FormattedDate;
 export const FormattedRelative = RI.FormattedRelative;
@@ -32,12 +29,18 @@ export const FormattedTime = RI.FormattedTime;
 export type InjectedIntl = RI.InjectedIntl<IDS>;
 export type InjectedIntlProps = RI.InjectedIntlProps<IDS>;
 
+export type TranslatorFunction = (stringId: IDS, values?: Object) => string;
+
 /** * Given an InjectedIntl object return a function that can be used for getting
  * internationalized string constants with less typing.
  *
  *  Usage: __ = bindStr(this.props.intl);
  *  console.log(__("string-key"));
  */
-export function bindStr(intl: RI.InjectedIntl<IDS>) {
-    return (stringId: IDS) => intl.formatMessage({ id: stringId });
+export function bindStr(intl?: RI.InjectedIntl<IDS>) {
+    if (intl) {
+        return (stringId: IDS, values?: Object) => intl.formatMessage({ id: stringId }, values);
+    } else {
+        return (stringId: IDS) => stringId;
+    }
 }
