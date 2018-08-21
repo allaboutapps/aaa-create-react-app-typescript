18d928d01c2021a9f44d8e9b7160eeb07dcd6e67
Christoph Werner <christoph@chrztoph.com>
2018-08-21 16:20:43 +0200

    Replace mui theme

---
+++ packages/react-scripts/template/src/components/util/Theme.tsx
@@ -0,0 +1,27 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { Styles } from "./Styles";
+
+// tslint:disable-next-line
+const theme = createMuiTheme({
+  // palette: {
+  //   primary: {
+  //     main: Styles.MAIN_COLOR
+  //   },
+  //   secondary: {
+  //     main: Styles.MAIN_COLOR
+  //   }
+  // },
+  // overrides: {
+  //   MuiButton: {
+  //     contained: {
+  //       backgroundColor: Styles.MAIN_COLOR,
+  //       color: "white",
+  //       "&:hover": {
+  //         backgroundColor: Styles.MAIN_COLOR_HOVERED
+  //       }
+  //     }
+  //   }
+  // }
+});
+
+export { theme };
