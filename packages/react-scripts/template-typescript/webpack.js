69484903790b996424102d8061ad05d0afd724bf
Gottfried Chen <gottfried.chen@allaboutapps.at>
2019-11-19 10:14:56 +0100

    add licencs checker plugin

--- packages/react-scripts/template-typescript/webpack.js
+++ packages/react-scripts/template-typescript/webpack.js
@@ -1,3 +1,5 @@
+const LicenseCheckerWebpackPlugin = require('license-checker-webpack-plugin');
+
 // Allows to manipulate the webpack config in aaa-react-scripts-ts
 // without the need to eject the project. Use this as your last resort.
 // * ./node_modules/aaa-react-scripts-ts/config/webpack.config.dev.js
@@ -7,6 +9,11 @@ module.exports = function getConfig(config, isProduction) {
     // don't parse the localForage package.
     // https://github.com/localForage/localForage#browserify-and-webpack
     // config.module.noParse = /node_modules\/localforage\/dist\/localforage.js/;
+
+    const licenseChecker = new LicenseCheckerWebpackPlugin({
+        outputFilename: 'ThirdPartyNotices.txt',
+    });
+    config.plugins.push(licenseChecker);
 
     return config;
 };
